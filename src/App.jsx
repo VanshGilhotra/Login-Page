7503f5a649ba3004482c8f65a8a3db7f5400901f
VanshGilhotra <vanshgilhotra00992@gmail.com>
2025-02-14 20:37:23 +0530

    All Errors Solved

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  //All the variables declared using use state For Login and signup
   const [isLogin, setLogin] = useState(false);
   const [info, setInfo] = useState([]);
   const [firstname, setfirstName] = useState('');
@@ -15,8 +17,10 @@ function App() {
   const [loginMail, getMail] = useState('');
   const [loginPass, getPass] = useState('');
 
+  //function created to signup and add user in Background for Login
   function onSubmit() {
 
+    //created new object of Signup Details and adding it for Login Credential
     const newsignUp = {
       Firstname: firstname,
       LastName: lastname,
@@ -24,15 +28,18 @@ function App() {
       Pass: password,
       CPass: confirmPassword
     };
-    
-    if (newsignUp.Pass === newsignUp.CPass) {
+  
+    //Check Condition if Username Does'nt Exist and Password is same as Confirm Password Field 
+    if (newsignUp.Pass === newsignUp.CPass && !info.some(user => user.Firstname === newsignUp.Firstname)) {
       setInfo([...info, newsignUp]);
+      
       alert("SignUp Successfully");
     } else {
-      alert("Login Details Are Invalid");
+      alert("Signup Details Are Invalid || Username Already Exist");
     }
   };
 
+  //Fucntion For LogIn with checking condition that email and password is correct
   function onLogin() {
     if (email === loginMail && password === loginPass) {
       alert("Login Successfull");
@@ -40,42 +47,38 @@ function App() {
     else {
       alert("Invalid Login Credentials");
     }
-  }
+  };
   
-  const HandleConfirmation = (event) => {
-    if (event.target.value == true) {
-      setLogin(false);
-    }
-    else {
-      setLogin(true);
-    }
-    console.log(event.target.value);
-}
-
-
+//Now the Html Starts to Display Either Login Page Or Sign Up as per Requirement
   return (
     <>
       {isLogin? <h1>Welcome Back</h1>:<h1>Sign Up</h1>}
 
       {isLogin ? (<div id='login_page'>
-          <input type="email" placeholder='Enter Email' onChange={(detail) => getMail(detail.target.value)}/><br />
-          <input type="password" placeholder='Enter Password' onChange={(detail) => getPass(detail.target.value)}/>
-
+        
+          <input type="email" placeholder='Enter Email' onChange={(detail) => getMail(detail.target.value)} required/><br />
+          <input type="password" placeholder='Enter Password' onChange={(detail) => getPass(detail.target.value)} required/>
+        
+          <br />
           <button onClick={() => onLogin()}>Login</button>
+        
           <p>Don't Have an Account <u onClick={() => setLogin(false)}>SignUp</u></p>
       </div>) :
+
         (<div className="signup">
-        <input type="text" placeholder='Enter First Name' onChange={(event) => (setfirstName(event.target.value))} />
-        <br />
-        <input type="text" placeholder='Enter Last Name' onChange={(event) => (setlastName(event.target.value))} />
-        <br />
-        <input type="email" placeholder='Enter Your Email' onChange={(event) => (setmail(event.target.value))} />
-        <br />
-        <input type="password" placeholder='Enter Password' onChange={(event) => (setpass(event.target.value))} />
-        <br />
-        <input type="password" placeholder='Confirm Password' onChange={(event) => (setConfirmation(event.target.value))} />
         
+        <input type="text" placeholder='Enter First Name' onChange={(event) => (setfirstName(event.target.value))} required/>
+        <br />
+        <input type="text" placeholder='Enter Last Name' onChange={(event) => (setlastName(event.target.value))} required/>
+        <br />
+        <input type="email" placeholder='Enter Your Email' onChange={(event) => (setmail(event.target.value))} required/>
+        <br />
+        <input type="password" placeholder='Enter Password' onChange={(event) => (setpass(event.target.value))} required/>
+        <br />
+        <input type="password" placeholder='Confirm Password' onChange={(event) => (setConfirmation(event.target.value))} required/>
+        <br />
         <button onClick={() => onSubmit()}>Submit</button>
+        
         <p>Already Have an Account <u onClick={() => setLogin(true)}>Login</u></p>
       </div>)}
       
